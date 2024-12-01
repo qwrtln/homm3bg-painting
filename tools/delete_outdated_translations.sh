65dbbdf5b5d7a92c491e3456e99968c6f5245311
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2024-12-01 18:40:04 +0100

    Delete leftovers

--- tools/delete_outdated_translations.sh
+++ tools/delete_outdated_translations.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 for f in localizations/*; do
-  if [[ -z $(find docs/ -name "$f") ]]; then
+  if [[ -z $(find docs/ -wholename "$f") ]]; then
     echo "Not found original files for $f, deleting"
     rm -rf "localizations/$f"
   fi
